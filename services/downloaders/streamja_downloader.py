70eadeb32d8566f7725325ef6880303f7b86b5e0
Mate Bersenadze <berse911@gmail.com>
2021-11-02 23:06:45 +0400

    fix streamja

--- services/downloaders/streamja_downloader.py
+++ services/downloaders/streamja_downloader.py
@@ -8,4 +8,9 @@ class StreamjaDownloader:
     def get_video_content(url):
         response = requests.get(url)
         html = fromstring(response.text)
-        return requests.get(html.xpath("//video/source/@src")[0]).content
+        retries = 10
+        while retries:
+            try:
+                return requests.get(html.xpath("//video/source/@src")[0]).content
+            except requests.exceptions.SSLError or requests.exceptions.ConnectionError:
+                retries -= 1
