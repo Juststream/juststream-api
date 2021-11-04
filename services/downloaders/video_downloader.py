aafcbdd96fc4208d6792b705943237d18c9318cb
Mate Bersenadze <berse911@gmail.com>
2021-11-04 15:09:20 +0400

    refactor video downloader

--- services/downloaders/video_downloader.py
+++ services/downloaders/video_downloader.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 import requests
 
 from helpers.url_helper import get_website_host
@@ -13,6 +15,23 @@ from services.downloaders.streamye_downloader import StreamyeDownloader
 from services.downloaders.twitter_downloader import TwitterDownloader
 from services.downloaders.youtube_downloader import YoutubeDownloader
 
+VIDEO_DOWNLOADER_MAP: Final = {
+    'reddit.com': RedditDownloader,
+    'v.redd.it': RedditDownloader,
+    'redd.it': RedditDownloader,
+    'streamable.com': StreamableDownloader,
+    'twitter.com': TwitterDownloader,
+    'youtube.com': YoutubeDownloader,
+    'youtu.be': YoutubeDownloader,
+    'streamwo.com': StreamwoDownloader,
+    'streamja.com': StreamjaDownloader,
+    'clippituser.tv': ClippituserDownloader,
+    'streamye.com': StreamyeDownloader,
+    'gfycat.com': GfycatDownloader,
+    'scuffedentertainment.com': ScuffedentertainmentDownloader,
+    'streamff.com': StreamffDownloader
+}
+
 
 class VideoDownloader:
 
@@ -23,25 +42,6 @@ class VideoDownloader:
     def get_video_content(self):
         if self.url.split('?')[0].endswith('.mp4') or self.url.split('?')[0].endswith('.ts'):
             return requests.get(self.url).content
-        elif self.website_host == 'reddit.com' or self.website_host == 'redd.it' or self.website_host == 'v.redd.it':
-            return RedditDownloader().get_video_content(self.url)
-        elif self.website_host == 'streamable.com':
-            return StreamableDownloader().get_video_content(self.url)
-        elif self.website_host == 'twitter.com':
-            return TwitterDownloader().get_video_content(self.url)
-        elif self.website_host == 'youtube.com' or self.website_host == 'youtu.be':
-            return YoutubeDownloader().get_video_content(self.url)
-        elif self.website_host == 'streamwo.com':
-            return StreamwoDownloader().get_video_content(self.url)
-        elif self.website_host == 'streamja.com':
-            return StreamjaDownloader().get_video_content(self.url)
-        elif self.website_host == 'clippituser.tv':
-            return ClippituserDownloader().get_video_content(self.url)
-        elif self.website_host == 'streamye.com':
-            return StreamyeDownloader().get_video_content(self.url)
-        elif self.website_host == 'gfycat.com':
-            return GfycatDownloader().get_video_content(self.url)
-        elif self.website_host == 'scuffedentertainment.com':
-            return ScuffedentertainmentDownloader().get_video_content(self.url)
-        elif self.website_host == 'streamff.com':
-            return StreamffDownloader().get_video_content(self.url)
+        video_downloader = VIDEO_DOWNLOADER_MAP.get(self.website_host)
+        if video_downloader:
+            return video_downloader.get_video_content(self.url)
